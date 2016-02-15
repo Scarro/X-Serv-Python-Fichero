441033151227baa0f0b8945ee8e6d4fd2e036ed5
scarro <sergioca22@gmail.com>
2016-02-15 19:01:00 +0100

    Ejercicio 13.4 (y 13.5 comentado)

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fichero = open("/etc/passwd", "r");
+lineas = fichero.readlines();
+fichero.close();
+diccionario = {};
 
+for linea in lineas:
+	elemento = linea.split(":");
+	usuario = elemento[0];
+	shell = elemento[-1][:-1];
+	diccionario[usuario] = shell
+	print(usuario + ": " + shell);
+
+print("El numero de usuarios es " + str(len(diccionario)));
+
+#print("La shell de root es: " + diccionario["root"]);
+#try:
+#	print(diccionario["imaginario"]);
+#except:
+#	print "El nombre de usuario 'imaginario' no existe"
